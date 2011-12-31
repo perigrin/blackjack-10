01337428578c57673f20418258d681303699cc0b
Chris Prather <chris@prather.org>
2011-12-30 23:59:41 -0500

    add some pod

--- bj.pl
+++ bj.pl
@@ -24,7 +24,9 @@ sub value {
     $v;
 }
 sub show($+) { say sprintf "%s (%i)", "$_[0] @{$_[1]}", value( $_[1] ) }
+
 my ( $player, $dealer ) = map { deal( $_, 2 ) } ( [], [] );
+
 while ( prompt( "@$player\nHit? ", '-tyn1' ) ) {
     show( "Busted!", $player ) && exit if value( deal( $player, 1 ) ) > 21;
 }
@@ -37,12 +39,25 @@ value($player) > value($dealer)
     : show( "Dealer wins", $dealer );
 
 __END__
-# ten line version
-use 5.14.1; use IO::Prompt; use List::Util qw(shuffle);
-sub deal(+$) { state $shoe = [ shuffle map { my $c = $_; map {"$c$_"} qw(❤ ◆ ♣ ♠) } ( 2 .. 10, qw( J Q K A ) ) x 6 ]; push $_[0], shift $shoe for ( 1 .. $_[1] ); $_[0]; }
-sub value { my $v; for ( local @_ = @{ shift() } ) { s/[ ❤ ◆ ♣ ♠ ]//; s/[JQK]/10/; $v < 11 ? s/A/11/ : s/A/1/; $v += $_; } $v; }
-sub show($+) { say sprintf "%s (%i)", "$_[0] @{$_[1]}", value( $_[1] ) }
-my ( $player, $dealer ) = map { deal( $_, 2 ) } ( [], [] );
-while ( prompt( "@$player\nHit? ", '-tyn1' ) ) { show( "Busted!", $player ) && exit if value( deal( $player, 1 ) ) > 21;}
-while ( say("Dealer @$dealer") && value($dealer) < 17 ) { show( "Dealer busted!", $dealer ) && exit if value( deal( $dealer, 1 ) ) > 21; }
-value($player) > value($dealer) ? show( "Player wins", $player ) : show( "Dealer wins", $dealer );
+
+=head1  A 10 Line Black Jack
+
+So the above is based on wanting a 10 line version of Black Jack based upon
+the answers to a question on stack overflow[1]
+here is the 10 line version
+
+    use 5.14.1; use IO::Prompt; use List::Util qw(shuffle);
+    sub deal(+$) { state $shoe = [ shuffle map { my $c = $_; map {"$c$_"} qw(❤ ◆ ♣ ♠) } ( 2 .. 10, qw( J Q K A ) ) x 6 ]; push $_[0], shift $shoe for ( 1 .. $_[1] ); $_[0]; }
+    sub value { my $v; for ( local @_ = @{ shift() } ) { s/[ ❤ ◆ ♣ ♠ ]//; s/[JQK]/10/; $v < 11 ? s/A/11/ : s/A/1/; $v += $_; } $v; }
+    sub show($+) { say sprintf "%s (%i)", "$_[0] @{$_[1]}", value( $_[1] ) }
+    my ( $player, $dealer ) = map { deal( $_, 2 ) } ( [], [] );
+    while ( prompt( "@$player\nHit? ", '-tyn1' ) ) { show( "Busted!", $player ) && exit if value( deal( $player, 1 ) ) > 21;}
+    while ( say("Dealer @$dealer") && value($dealer) < 17 ) { show( "Dealer busted!", $dealer ) && exit if value( deal( $dealer, 1 ) ) > 21; }
+    value($player) > value($dealer) ? show( "Player wins", $player ) : show( "Dealer wins", $dealer );
+
+=head1 Spot the Bug
+
+There is a subtle bug in teh logic (plus probably a thousand other unsubtle
+ones I have no clue about). I would love it if someone could fix it.
+
+[1]: http://stackoverflow.com/questions/811074/what-is-the-coolest-thing-you-can-do-in-10-lines-of-simple-code-help-me-inspir 
