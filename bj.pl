c0c6b15787b11ea9357a849c7e1e417027726c4f
Chris Prather <chris@prather.org>
2024-08-17 01:04:51 -0400

    Update bj.pl to 5.36.1
    Rewrite this to keep up with changes in the last 8 years.

--- bj.pl
+++ bj.pl
@@ -1,50 +1,44 @@
-use 5.14.1;
-use IO::Prompt;
-use List::Util qw(shuffle);
+use 5.36.1;
+use IO::Prompt   qw( prompt );
+use List::Util   qw( reduce shuffle );
+use Set::Product qw( product );
 
-sub deal {
-    state $shoe = [
-        shuffle map {
-            my $v = $_;
-            map {"$v$_"} qw(❤ ◆ ♣ ♠)
-            } ( 2 .. 10, qw( J Q K A ) ) x 6
-    ];
-    push $_[0], splice( $shoe, 0, $_[1] );
-    $_[0];
+sub new_shoe( $n = 6 ) {
+    my @d = ();
+    product { push @d, "@_" } [ ( 2 .. 10, qw( J Q K A ) ) ], [qw(❤ ◆ ♣ ♠)];
+    shuffle( (@d) x $n );
 }
 
-sub value {
-    my $v;
-    for ( local @_ = @{ shift() } ) {
-        s/[ ❤ ◆ ♣ ♠ ]//;
-        s/[JQK]/10/;
-        $v < 11 ? s/A/11/ : s/A/1/;
-        $v += $_;
-    }
-    $v;
+sub deal( $hand, $n ) {
+    state @shoe = new_shoe();
+    push @$hand, splice( @shoe, 0, $n );
+    $hand;
 }
 
-sub show { say sprintf "%s (%i)", "$_[0] @{$_[1]}", value( $_[1] ) }
+sub value($hand) {
+    reduce { $a += ( "$b" ne 'A' ? $b : $a < 11 ? 1 : 11 ) }
+    map { s/[ ❤ ◆ ♣ ♠ ]//g; s/[JQK]/10/r; } [@$hand]->@*;
+}
 
-my ( $player, $dealer ) = map { deal( $_, 2 ) } ( [], [] );
+sub show( $msg, $hand ) {
+    printf "%s (%i)\n", "$msg @$hand", value($hand);
+    exit 0;
+}
+
+my ( $player, $dealer ) = map deal( $_, 2 ), ( [], [] );
+
+say "Dealer shows: $dealer->[0]";
 
 while ( prompt( "@$player\nHit? ", '-tyn1' ) ) {
-    if ( value( deal( $player, 1 ) ) > 21 ) {
-        show( "Busted!", $player );
-        exit;
-    }
+    show( "Busted!", $player ) if value( deal( $player, 1 ) ) > 21;
 }
 
 while ( say("Dealer @$dealer") && value($dealer) < 17 ) {
-    if ( value( deal( $dealer, 1 ) ) > 21 ) {
-        show( "Dealer busted!", $dealer );
-        exit;
-    }
+    show( "Busted!", $dealer ) if value( deal( $dealer, 1 ) ) > 21;
 }
 
 value($player) >= value($dealer)
-    ? show( "Player wins", $player )
-    : show( "Dealer wins", $dealer );
+  ? show( "Player wins", $player )
+  : show( "Dealer wins", $dealer );
 
 __END__
-
